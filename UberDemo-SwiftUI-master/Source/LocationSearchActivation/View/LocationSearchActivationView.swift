5d47e01c9918f6d8b17447ba7498232f70dbf65e
Viktor Velizarov <102257718+Viktor1722@users.noreply.github.com>
2023-05-22 21:29:26 +0200

    Add files via upload

---
+++ UberDemo-SwiftUI-master/Source/LocationSearchActivation/View/LocationSearchActivationView.swift
@@ -0,0 +1,38 @@
+//
+//  LocationSearchActivationView.swift
+//  Uber-SwiftUI (iOS)
+//
+//  Created by Bhoopendra Umrao on 4/12/23.
+//
+
+import SwiftUI
+
+struct LocationSearchActivationView: View {
+    @Binding var mapState: MapState
+    
+    let TextWhere: LocalizedStringKey = "TextWhere"
+    
+    var body: some View {
+        HStack {
+            Rectangle()
+                .fill(Color.black)
+                .frame(width: 8, height: 8)
+                .padding(.horizontal)
+            Text(TextWhere)
+                .foregroundColor(Color(.darkGray))
+            Spacer()
+        }
+        .frame(width: UIScreen.main.bounds.width - 64, height: 50)
+        .background(
+            Rectangle()
+                .fill(Color.white)
+                .shadow(color: .black, radius: 6)
+        )
+        .onTapGesture {
+            withAnimation(.spring()) {
+                mapState = .locationSearch
+            }
+        }
+    }
+}
+
